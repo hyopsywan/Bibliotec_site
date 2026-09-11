e90c25866fb8344c4b6bebb922e767e9ed2e553c
HELOÍSA TOMÉ DE ARAUJO <52577710801@info.local>
2026-09-11 11:00:42 -0300

    :books: Docs: Testes com js

---
+++ Home/script.js
@@ -0,0 +1,59 @@
+// let nome = "Hello";
+// let idade = 20;
+
+// console.log("Nome:" + nome ); //concatenação
+
+// console.log(`Nome: ${nome}, idade: ${idade}`); //interpolação
+
+// console.log(typeof nome);
+// console.log(typeof idade);
+
+// // Teste de resultados 
+// let soma = "5" + 5; // concatena
+// let sub = "5" - 5, // soma
+
+// console.log(soma);
+// console.log(sub);
+
+// console.log(typeof soma); //string
+// console.log(typeof sub); //number 
+
+// //teste de permissividade
+// let number = "ab";
+// let convertido = Number(number);
+
+// console.log(typeof number);
+// console.log(number);
+
+// // Operadores relacionais 
+
+// "10" == 10 -> true;  // Igualdade no valor
+// "10" === 10 -> false; // Igualdade de valor e tipo de dado
+// let 10 = 10; // Recebe 
+
+// //Operadores lógicos
+// && // E
+// || // OU
+// ! // Negação
+
+// // SWITCH CASE
+// let opcao = 2
+
+// switch(opcao) {
+//     case 1: TextDecoderStream;
+//         break;
+// }
+
+// ESTRUTURA DE REPETIÇÃO
+
+// FOR EACH - forma mais facil e rapida de percorrer um array:
+// let frutas = ["Maçã", "Banana", "Acerola", "Uva"]
+
+// console.log(frutas);
+
+// frutas.forEach(function(fruta){
+//     console.log("Nome:" + fruta);
+// })
+
+// //OU usando arrow function para diminuir a função for each em uma linha
+// frutas.forEach(fruta => console.log("Nome:" + fruta))
